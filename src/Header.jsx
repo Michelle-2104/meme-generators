a5d3cbbba92d18afc1377dca76458cdcc5d57042
Michelle2104 <chidowemichelle@gmail.com>
2022-12-28 19:18:02 +0200

    .

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import troll from "./img/troll-face.png"
+
+export default function Header() {
+  return (
+    <div>
+        <nav>
+            <img className='nav--image' src={troll} alt='animated funny face' ></img>
+        <h1 className='nav--title'>Meme Generator</h1>
+        <h3 className='nav--proj'>React Project 3</h3>
+        </nav>
+    </div>
+  )
+}
---
+++ src/Meme.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Meme() {
+  return (
+    <div className='container'>
+        <form className='form'>
+            <input type='text' className='form--input'></input>
+            <input type='text' className='form--input'></input>
+            <button type='text' className='form--button'>Get a new meme image</button>
+        </form>
+     </div>
+  )
+}
