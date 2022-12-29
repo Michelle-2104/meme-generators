5605cf4974e0d9c5a64877d3fb572d042964b1bc
Michelle2104 <chidowemichelle@gmail.com>
2022-12-29 17:02:56 +0200

    .

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import troll from "./img/troll-face.png"
 export default function Header() {
   return (
     <div>
-        <nav>
+        <nav className='navbar'>
             <img className='nav--image' src={troll} alt='animated funny face' ></img>
         <h1 className='nav--title'>Meme Generator</h1>
         <h3 className='nav--proj'>React Project 3</h3>
